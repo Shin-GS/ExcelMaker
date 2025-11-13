31c5b91e5ee8fa0bd613fde909a4d65ab2e1b81e
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-11-13 15:11:56 +0900

    feat: 라이브러리 배포
    패키지명 변경
    github 주소 기준으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
-
-    // Optional but recommended: also publish sources and javadoc jars
-    withSourcesJar()
-    withJavadocJar()
 }
 
 group = "io.github.shin-gs"
@@ -29,10 +27,41 @@ tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.shin-gs",
+        artifactId = "excelmaker",
+        version = "0.0.1"
+    )
+
+    pom {
+        name.set("ExcelMaker")
+        description.set("A lightweight Java library for generating Excel and CSV files.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Shin-GS/ExcelMaker")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("shin-gs")
+                name.set("Shin GS")
+                email.set("rudtjq1213@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Shin-GS/ExcelMaker")
+            connection.set("scm:git:https://github.com/Shin-GS/ExcelMaker.git")
+            developerConnection.set("scm:git:git@github.com:Shin-GS/ExcelMaker.git")
         }
     }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
